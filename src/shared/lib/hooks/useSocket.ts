5f500c9c51ba7063f4aa86a1458b4441ec0f0476
Vlad AStahov <vlad-astahov@mail.ru>
2024-08-08 00:09:40 +0300

    try to fix reconnect error

--- src/shared/lib/hooks/useSocket.ts
+++ src/shared/lib/hooks/useSocket.ts
@@ -1,15 +1,24 @@
-import useWebSocket from "react-use-websocket";
+import { useEffect, useRef } from "react";
+import useWebSocket, { ReadyState } from "react-use-websocket";
+
+let isFirstConnection = false
 
 export const useSocket = () => {
-    const { sendMessage, lastMessage } = useWebSocket(
+    const { sendMessage, lastMessage, readyState } = useWebSocket(
         'wss://socket.toptubereviews.buzz',
         {
             shouldReconnect: () => true,
             reconnectInterval: 0,
             onOpen: () => {
-                const token = localStorage.getItem('jwt-token')
-                sendMessage(`handshake:{"jwt_token":"${token}"}`)
+                if (!isFirstConnection) {
+                    const token = localStorage.getItem('jwt-token')
+                    sendMessage(`handshake:{"jwt_token":"${token}"}`)
+                    isFirstConnection = true
+                }
             },
+            onClose: () => {
+                isFirstConnection = false
+            }
         }
     )
 
