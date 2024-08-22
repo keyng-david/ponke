5baa0d5c315c700444924e72dbe31b92a737b455
keyng-david <61462535+keyng-david@users.noreply.github.com>
2024-08-22 10:44:49 +0100

    Update user.ts

--- api/user.ts
+++ api/user.ts
@@ -1,10 +1,13 @@
 import { createClient } from '@supabase/supabase-js';
 import { verifyToken } from './auth';
 
-const supabase = createClient(
-  process.env.SUPABASE_URL!,
-  process.env.SUPABASE_KEY!
-);
+
+const supabaseUrl = process.env.SUPABASE_URL!;
+
+const supabaseKey = process.env.SUPABASE_KEY!;
+
+const supabase = createClient(supabaseUrl, supabaseKey);
+
 
 export const getUserInfo = async (token: string) => {
   const userId = verifyToken(token);
