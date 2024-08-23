fb79fc9913c5a4b1808d24240e597972b095fc3c
keyng-david <61462535+keyng-david@users.noreply.github.com>
2024-08-23 16:23:14 +0100

    Update createRequest.ts

--- src/shared/lib/api/createRequest.ts
+++ src/shared/lib/api/createRequest.ts
@@ -1,24 +1,12 @@
-export type SuccessResponse<T> = {
-    error: false,
-    payload: T
-}
-
-export type FailureResponse = {
-    error: true,
-    payload: null
-}
-
-export type ResponseDefault<T> = SuccessResponse<T> | FailureResponse
-
 export async function createRequest<T>(data: {
     url: string
     method: 'POST' | 'GET' | 'PUT' | 'DELETE',
     data?: Record<string, unknown>,
 }): Promise<ResponseDefault<T>> {
     try {
-        const token = await localStorage.getItem('jwt-token')
+        const token = await localStorage.getItem('jwt-token');
 
-        const url = `https://ponke-alpha.vercel.app/api/${data.url}`;
+        const url = `/api/${data.url}`;
 
         const response = await fetch(
             url,
@@ -32,25 +20,25 @@ export async function createRequest<T>(data: {
                     body: JSON.stringify(data.data)
                 })
             }
-        )
+        );
 
-        const payload = await response.json() as T
+        const payload = await response.json() as T;
 
         if (response.ok) {
             return {
                 error: false,
-                payload,
-            }
+                payload
+            };
+        } else {
+            return {
+                error: true,
+                payload: null
+            };
         }
-
+    } catch (error) {
         return {
             error: true,
             payload: null
-        }
-    } catch (e) {
-        return {
-            error: true,
-            payload: null
-        }
+        };
     }
 }
