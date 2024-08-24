7f62977903c0d682ee8d3d7af8bdc5bf6d4c0504
keyng-david <61462535+keyng-david@users.noreply.github.com>
2024-08-24 13:24:29 +0100

    Update useAuth.ts

--- src/features/auth/useAuth.ts
+++ src/features/auth/useAuth.ts
@@ -1,69 +1,68 @@
-import { useCallback, useState } from "react";
+import { useCallback } from "react";
 import { useNavigate } from "react-router-dom";
-import { clickerModel } from '../clicker/model';
+import { clickerModel } from "../clicker/model";
 import { useJWTToken } from "@/shared/model/jwt";
 import { createRequest } from "@/shared/lib/api/createRequest";
 import { createEvent, createStore } from "effector";
 import { useUnit } from "effector-react";
 import { walletModel } from "@/shared/model/wallet";
 import { randModel } from "@/shared/model/rang";
+import { useErrorHandler } from "@/shared/lib/hooks/useErrorHandler";
 
 const setIsAuth = createEvent<boolean>();
 
 const $isAuth = createStore(false).on(setIsAuth, () => true);
 
 export const useAuth = () => {
-    const navigate = useNavigate();
-    const isAuth = useUnit($isAuth);
-    const jwtTokenStore = useJWTToken();
-    const wallet = walletModel.useWalletModel();
-    const rangModel = randModel.useRang();
-    const [error, setError] = useState<string | null>(null);
+  const navigate = useNavigate();
+  const isAuth = useUnit($isAuth);
+  const jwtTokenStore = useJWTToken();
+  const wallet = walletModel.useWalletModel();
+  const rangModel = randModel.useRang();
+  const { setError } = useErrorHandler();
 
-    const initialize = useCallback(async () => {
-        try {
-            if (!isAuth) {
-                const urlParams = new URLSearchParams(window.location.search);
-                const token = urlParams.get('token')
+  const initialize = useCallback(async () => {
+    try {
+      if (!isAuth) {
+        const urlParams = new URLSearchParams(window.location.search);
+        const token = urlParams.get("token");
 
-                if (token) {
-                    jwtTokenStore.set(token);
-                } else {
-                    setError("No token found");
-                    return;
-                }
-
-                const response = await createRequest<{
-                    score: number;
-                    available_clicks: number;
-                    wallet: string;
-                    level: number;
-                }>({
-                    url: 'game/auth',
-                    method: 'POST',
-                });
-
-                if (!response.error) {
-                    clickerModel.valueInited(response.payload.score);
-                    clickerModel.availableInited(response.payload.available_clicks);
-                    wallet.updateWallet(response.payload.wallet);
-                    rangModel.update(response.payload.level);
-                    navigate('/main');
-                    setIsAuth(true);
-                } else {
-                    setError("Authentication failed. Please try again.");
-                    jwtTokenStore.remove();
-                }
-            }
-        } catch (e: any) { // Type assertion to any to safely access properties
-            const errorMessage = e instanceof Error ? e.message : String(e);
-            setError(`Error during authentication: ${errorMessage}`);
-            jwtTokenStore.remove();
+        if (token) {
+          jwtTokenStore.set(token);
+        } else {
+          setError("No token found");
+          return;
         }
-    }, [isAuth, jwtTokenStore, wallet, rangModel, navigate]);
 
-    return {
-        initialize,
-        error,
-    };
+        const response = await createRequest<{
+          score: number;
+          available_clicks: number;
+          wallet: string;
+          level: number;
+        }>({
+          url: "game/auth",
+          method: "POST",
+        });
+
+        if (!response.error) {
+          clickerModel.valueInited(response.payload.score);
+          clickerModel.availableInited(response.payload.available_clicks);
+          wallet.updateWallet(response.payload.wallet);
+          rangModel.update(response.payload.level);
+          navigate("/main");
+          setIsAuth(true);
+        } else {
+          jwtTokenStore.remove();
+          setError("Authentication failed, invalid response");
+        }
+      }
+    } catch (e) {
+      jwtTokenStore.remove();
+      setError(`Error during authentication: ${e.message}`);
+    }
+  }, [isAuth, jwtTokenStore, wallet, rangModel, navigate, setError]);
+
+  return {
+    initialize,
+  };
 };
