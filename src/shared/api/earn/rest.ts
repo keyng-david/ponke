f217499b576263d1cc4ddc7c96e1bf5d7712df5c
keyng-david <61462535+keyng-david@users.noreply.github.com>
2024-08-25 14:39:56 +0100

    Update rest.ts

--- src/shared/api/earn/rest.ts
+++ src/shared/api/earn/rest.ts
@@ -2,14 +2,17 @@ import { EarnApi } from './types'
 import {createRequest} from "@/shared/lib/api/createRequest";
 
 export const earnApi: EarnApi = {
-    getData: async () => await createRequest({
-        endpoint: 'game/tasks',
-        method: 'GET',
+  getData: async () =>
+    await createRequest({
+      endpoint: 'game/tasks',
+      method: 'GET',
+      // onError is omitted if not needed
     }),
-    taskJoined: async body => await createRequest({
-        endpoint: 'game/completeTask',
-        method: "POST",
-        body,
-        onError: undefined // or provide a function
-    })
-}
+  taskJoined: async (body) =>
+    await createRequest({
+      endpoint: 'game/completeTask',
+      method: 'POST',
+      body,
+      // onError is omitted if not needed
+    }),
+};
