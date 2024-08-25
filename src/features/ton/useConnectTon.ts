41f5059c67bac87ac884bc12ddc9941700c25b65
keyng-david <61462535+keyng-david@users.noreply.github.com>
2024-08-25 13:39:51 +0100

    Update useConnectTon.ts

--- src/features/ton/useConnectTon.ts
+++ src/features/ton/useConnectTon.ts
@@ -43,7 +43,7 @@ export const useConnectTon = () => {
     ) {
         try {
             const checkResponse = await createRequest({
-                url: 'wallet/checkProof',
+                endpoint: 'wallet/checkProof',
                 method: 'POST',
                 data: {
                     address: account.address,
