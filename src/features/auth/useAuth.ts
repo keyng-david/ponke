58bc7a49751ac62c9bcc25d09b8a256bc493edcb
keyng-david <61462535+keyng-david@users.noreply.github.com>
2024-09-01 21:56:30 +0100

    Update useAuth.ts

--- src/features/auth/useAuth.ts
+++ src/features/auth/useAuth.ts
@@ -58,7 +58,7 @@ export const useAuth = () => {
         });
 
         if (!response.error) {
-          // Set initial score and available clicks from the backend response
+          // Initialize clickerModel with backend response
           clickerModel.valueInited(response.payload.score);
           clickerModel.availableInited(response.payload.available_clicks);
 
