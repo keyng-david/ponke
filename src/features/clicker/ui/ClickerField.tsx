ea14da337cd6d523c70fcf727db64caae3cbb2b9
keyng-david <61462535+keyng-david@users.noreply.github.com>
2024-09-03 12:52:58 +0100

    Update ClickerField.tsx

--- src/features/clicker/ui/ClickerField.tsx
+++ src/features/clicker/ui/ClickerField.tsx
@@ -11,8 +11,8 @@ import { useStore } from "effector-react";
 import { useGameData } from "@/shared/lib/hooks/useGameData"; // Import the custom hook
 
 export const ClickerField = () => {
-    const score = useStore(clickerModel.$value) ?? 0;
-  const availableClicks = useStore(clickerModel.$available) ?? 500;
+    const score = useStore(clickerModel.$value) ?? '';
+    const availableClicks = useStore(clickerModel.$available) ?? '';
   const canBeClicked = clickerModel.useCanBeClicked();
   const { haptic } = useTelegram();
 
