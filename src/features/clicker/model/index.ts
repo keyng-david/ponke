954b0793b3d4f357b1993d47fa2ee326be17658b
keyng-david <61462535+keyng-david@users.noreply.github.com>
2024-09-03 07:34:22 +0100

    Update index.ts

--- src/features/clicker/model/index.ts
+++ src/features/clicker/model/index.ts
@@ -141,8 +141,8 @@ export const useClicker = () => {
 // Export model object with all events, stores, and hooks
 export const clickerModel = {
   availableUpdated,
-  valueinited,
-  availableinited,
+  valueInited,
+  availableInited,
   clicked,
   errorUpdated,
   useCanBeClicked,
