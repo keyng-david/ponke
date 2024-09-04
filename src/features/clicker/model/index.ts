91e8ef82a87cda9888ea191110a251a3488d8a6d
keyng-david <61462535+keyng-david@users.noreply.github.com>
2024-09-04 14:42:47 +0100

    Update index.ts

--- src/features/clicker/model/index.ts
+++ src/features/clicker/model/index.ts
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback } from "react";
+import { useState, useEffect, useCallback, useRef } from "react";
 import { createEvent, createStore, sample } from "effector";
 import { useUnit } from "effector-react";
 import { $sessionId } from "@/shared/model/session";
