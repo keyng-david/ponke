1ab3e6208a83b25c118249e08ac362cba922f8b4
keyng-david <61462535+keyng-david@users.noreply.github.com>
2024-09-06 16:54:30 +0100

    Update ClickerField.tsx

--- src/features/clicker/ui/ClickerField.tsx
+++ src/features/clicker/ui/ClickerField.tsx
@@ -1,129 +1,128 @@
-import React, { TouchEvent, useCallback, useMemo, useState, useEffect } from "react";
-import { MAX_AVAILABLE, clickerModel } from "../model";
-import progress from "@/shared/assets/images/main/progress.png";
-import pointImage from "@/shared/assets/images/main/point.png";
-import leftHand from "@/shared/assets/images/main/left-hand.png";
-import rightHand from "@/shared/assets/images/main/right-hand.png";
-import styles from "./ClickerField.module.scss";
-import { getRandomArbitrary, getRandomInt, toFormattedNumber } from "@/shared/lib/number";
-import { useTelegram } from "@/shared/lib/hooks/useTelegram";
+import React, {TouchEvent, useCallback, useMemo, useState} from "react";
 
-let timeout1: NodeJS.Timeout;
-let timeout2: NodeJS.Timeout;
+import progress from '@/shared/assets/images/main/progress.png'
+import pointImage from '@/shared/assets/images/main/point.png'
+import leftHand from '@/shared/assets/images/main/left-hand.png'
+import rightHand from '@/shared/assets/images/main/right-hand.png'
+
+import {MAX_AVAILABLE, clickerModel} from "../model";
+
+import styles from './ClickerField.module.scss'
+import {getRandomArbitrary, getRandomInt, toFormattedNumber} from "@/shared/lib/number";
+import {useTelegram} from "@/shared/lib/hooks/useTelegram";
+
+let timeout1: NodeJS.Timeout
+let timeout2: NodeJS.Timeout
 
 export const ClickerField = () => {
-    const { value, available, canBeClicked, onClick } = clickerModel.useClicker();
-    const { haptic } = useTelegram();
+    const { value, available, canBeClicked, onClick } = clickerModel.useClicker()
 
-    const [isClickEnabled, setIsClickEnabled] = useState(true);
-    const [leftClasses, setLeftClasses] = useState<string[]>([styles["hand-left"]]);
-    const [rightClasses, setRightClasses] = useState<string[]>([styles["hand-right"]]);
-    const valueString = useMemo(() => toFormattedNumber(value), [value]);
+    const { haptic } = useTelegram()
 
-    const handleScoreUpdate = useCallback(() => {
-        // Trigger the click event and send the updated score to the backend
-        const newScore = value + CLICK_STEP; // Increment score on each click
-        clickerModel.clicked({
-            score: newScore,
-            click_score: newScore,
-            available_clicks: available - CLICK_STEP,
-        });
+    const [isClickEnabled, setIsClickEnabled] = useState(true)
+    const [leftClasses, setLeftClasses] = useState<string[]>([styles['hand-left']])
+    const [rightClasses, setRightClasses] = useState<string[]>([styles['hand-right']])
 
-        // Send throttled score to the backend
-        clickerModel.sendPointsUpdate(newScore);
-    }, [value, available]);
+    const valueString = useMemo(() => toFormattedNumber(value), [value])
 
-    const onTouchStart = useCallback(
-        (e: TouchEvent<HTMLDivElement>) => {
-            if (isClickEnabled) {
-                for (let i = 0; i < Math.min(e.touches.length, 3); i++) {
-                    const { clientX, clientY } = e.touches[i];
-                    if (canBeClicked) {
-                        onClick(); // Update UI on click
-                        handleScoreUpdate(); // Trigger score update
+    const onTouchStart = useCallback((e: TouchEvent<HTMLDivElement>) => {
+        if (isClickEnabled) {
+            for (let i = 0; i < Math.min(e.touches.length, 3); i++) {
+                const { clientX, clientY } = e.touches[i]
+                if (canBeClicked) {
+                    onClick()
 
-                        const point = document.createElement("img");
-                        point.src = pointImage;
-                        point.alt = "point";
-                        point.style.transform = `rotate(${getRandomInt(-25, 25)}deg) scale(${getRandomArbitrary(0.8, 1.2)})`;
+                    const point = document.createElement('img')
+                    point.src = pointImage
+                    point.alt = 'point'
+                    point.style.transform = `rotate(${getRandomInt(-25, 25)}deg) scale(${getRandomArbitrary(0.8, 1.2)})`
 
-                        const pointParent = document.createElement("div");
-                        pointParent.appendChild(point);
-                        pointParent.style.top = `${clientY}px`;
-                        pointParent.style.left = `${clientX}px`;
-                        pointParent.className = styles.point;
+                    const pointParent = document.createElement('div')
+                    pointParent.appendChild(point)
+                    pointParent.style.top = `${clientY}px`
+                    pointParent.style.left = `${clientX}px`
+                    pointParent.className = styles.point
 
-                        document.querySelector("#clicker")!.appendChild(pointParent);
-                        haptic();
+                    document.querySelector('#clicker')!.appendChild(pointParent)
+                    haptic()
+                    const timeout1 = setTimeout(() => {
+                        document.querySelector('#clicker')!.removeChild(pointParent)
 
-                        const timeout1 = setTimeout(() => {
-                            document.querySelector("#clicker")!.removeChild(pointParent);
-                            clearTimeout(timeout1);
-                        }, 500);
+                        clearTimeout(timeout1)
+                    }, 500)
 
-                        if (leftClasses.length === 1 && rightClasses.length === 1) {
-                            setLeftClasses((prevState) => [...prevState, styles["hand-animated"]]);
-                            setRightClasses((prevState) => [...prevState, styles["hand-animated"]]);
-                            timeout2 = setTimeout(() => {
-                                setRightClasses([styles["hand-right"]]);
-                                setLeftClasses([styles["hand-left"]]);
-                                clearTimeout(timeout2);
-                            }, 350);
-                        }
+                    if (leftClasses.length === 1 && rightClasses.length === 1) {
+                        setLeftClasses(prevState => [...prevState, styles['hand-animated']])
+                        setRightClasses(prevState => [...prevState, styles['hand-animated']])
+                        timeout2 = setTimeout(() => {
+                            setRightClasses([styles['hand-right']])
+                            setLeftClasses([styles['hand-left']])
+
+                            clearTimeout(timeout2)
+                        }, 350)
                     }
                 }
-
-                setIsClickEnabled(false);
-                timeout1 = setTimeout(() => {
-                    setIsClickEnabled(true);
-                    clearTimeout(timeout1);
-                }, 150);
             }
-        },
-        [isClickEnabled, canBeClicked, haptic, handleScoreUpdate, leftClasses.length, rightClasses.length]
-    );
+
+            setIsClickEnabled(false)
+            timeout1 = setTimeout(() => {
+                setIsClickEnabled(true)
+
+                clearTimeout(timeout1)
+            }, 150)
+        }
+    }, [isClickEnabled ,canBeClicked, haptic, leftClasses.length, rightClasses.length])
+
+    function handleTouchMove(event: TouchEvent<HTMLDivElement>) {
+        event.preventDefault()
+    }
+
+    function handleTouchEnd(event: TouchEvent<HTMLDivElement>) {
+        event.preventDefault();
+    }
 
     return (
         <div
-            id="clicker"
+            id={'clicker'}
             className={styles.root}
             onTouchStart={onTouchStart}
-            onTouchMove={(event) => event.preventDefault()}
-            onTouchEnd={(event) => event.preventDefault()}
+            onTouchMove={handleTouchMove}
+            onTouchEnd={handleTouchEnd}
         >
             <p className={styles.value}>{valueString}</p>
-            <ProgressBar value={available} />
+            <p className={styles.value}>{valueString}</p>
+            <ProgressBar value={available}/>
             <div className={styles.hands}>
-                <img id="handLeft" className={leftClasses.join(" ")} src={leftHand} alt="left hand" />
-                <img id="handRight" className={rightClasses.join(" ")} src={rightHand} alt="right hand" />
+                <img id={'handLeft'} className={leftClasses.join(' ')} src={leftHand} alt={'left hand'}/>
+                <img id={'handRight'} className={rightClasses.join(' ')} src={rightHand} alt={'right hand'}/>
             </div>
         </div>
-    );
-};
+    )
+}
 
-const ProgressBar = React.memo<{ value: number }>(({ value }) => {
+const ProgressBar = React.memo<{
+    value: number
+}>(({value}) => {
     const list = useMemo(() => {
         let count = 0;
-        let curr = value;
+        let curr = value
 
         while (curr >= 0) {
-            count += 1;
-            curr -= MAX_AVAILABLE / 12;
+            count += 1
+            curr = curr - MAX_AVAILABLE / 12
         }
 
-        return count;
-    }, [value]);
+        return count
+    }, [value])
 
     return (
-        <div className={styles["progress-bar"]}>
+        <div className={styles['progress-bar']}>
             <span className={styles.available}>{value}</span>
             <div className={styles.row}>
-                {Array(list)
-                    .fill(1)
-                    .map((_, index) => (
-                        <img key={index} className={styles["item"]} src={progress} alt="progress" />
-                    ))}
+                {Array(list).fill(1).map((_, index) => (
+                    <img key={index} className={styles['item']} src={progress} alt={'progress'} />
+                ))}
             </div>
         </div>
-    );
-});
+    )
+})
